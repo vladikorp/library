a0c76ee14a62dcf26dd045437f799b30660c7133
C-3PO <chepiha.artemii@gmail.com>
2023-03-07 17:01:24 +0200

    added pinia

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,9 +1,13 @@
 import { createApp } from 'vue/dist/vue.esm-bundler';
+import { createPinia } from 'pinia'
 import './style.css'
 import App from './App.vue'
 import router from './router/router'
 
+const pinia = createPinia()
 const app = createApp(App)
+
+app.use(pinia)
 app.use(router)
 
 router.isReady().then(() => {
